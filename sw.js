9c8c51fc8ddab8915606434486a963d8b5069c5d
awmamadou394-ops <awmamadou394@gmail.com>
2026-09-13 01:41:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
+
 self.addEventListener('push', function(event) {
   var data = {};
   try {
